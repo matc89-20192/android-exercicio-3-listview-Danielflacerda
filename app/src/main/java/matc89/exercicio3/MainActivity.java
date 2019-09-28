05ceb7d411e941980c3490b788521a4f50852ada
Daniel Lacerda <daniel.felacerda@hotmail.com>
2019-09-28 01:41:14 -0300

    Botao adicionar nao adicionando

--- app/src/main/java/matc89/exercicio3/MainActivity.java
+++ app/src/main/java/matc89/exercicio3/MainActivity.java
@@ -3,6 +3,7 @@ package matc89.exercicio3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +11,8 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import static matc89.exercicio3.R.id.listView;
 
@@ -17,10 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnAdd;
     private Button btnRemove;
-    private EditText descricao;
-    private EditText prioridade;
+    private EditText edtdescricao;
+    private EditText edtprioridade;
     private ListView list;
-    private ArrayList<Tarefa> tarefas;
+    private ArrayList<Tarefa> tarefas = new ArrayList<>();
     private Adapter adapter;
 
 
@@ -32,18 +35,68 @@ public class MainActivity extends AppCompatActivity {
 
         btnAdd = (Button) findViewById(R.id.buttonAdicionar);
         btnRemove = (Button) findViewById(R.id.buttonRemover);
-        descricao = (EditText) findViewById(R.id.editDescricao);
-        prioridade = (EditText) findViewById(R.id.editPrioridade);
+        edtdescricao = (EditText) findViewById(R.id.editDescricao);
+        edtprioridade = (EditText) findViewById(R.id.editPrioridade);
         list = (ListView) findViewById(R.id.listView);
+        list.setAdapter(adapter);
+        adapter = new Adapter(this, tarefas);
 
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view,
+                                    int i, long l) {
+                Tarefa tarefa = (Tarefa) list.getItemAtPosition(i);
+                tarefas.remove(tarefa);
+                adapter.notifyDataSetChanged();
+            }
+        });
 
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, values);
     }
 
     public void addTask(View view)
     {
+        if(tarefas.isEmpty()){
+            btnRemove.setEnabled(true);
+        }
 
+        String descricao = edtdescricao.getText().toString();
+        int prioridade = Integer.parseInt(edtprioridade.getText().toString());
+
+        if(validarTarefa(descricao, prioridade)) {
+            Tarefa tarefa = new Tarefa(descricao, prioridade);
+            tarefas.add(tarefa);
+            adapter.notifyDataSetChanged();
+        }
+
+        sortArrayList();
+    }
+
+    public void onRemoveClick(View view) {
+        tarefas.remove(0);
+        adapter.notifyDataSetChanged();
+        if(tarefas.isEmpty()){
+            btnRemove.setEnabled(false);
+        }
+    }
+    public void sortArrayList(){
+        Collections.sort(tarefas, new Comparator<Tarefa>() {
+            @Override
+            public int compare(Tarefa task, Tarefa task1) {
+                if(task.getPrioridade() < task1.getPrioridade())
+                {
+                    return -1;
+                }
+                else if(task.getPrioridade() > task1.getPrioridade())
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        });
+        adapter.notifyDataSetChanged();
     }
     public boolean validarTarefa(String descricao, int prioridade) {
         Boolean aux = false;
@@ -52,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
             return aux;
         } else {
             for (Tarefa tarefa : tarefas) {
-                if (descricao.equals(tarefa.getDescricao())) {
+                if (edtdescricao.equals(tarefa.getDescricao())) {
                     Toast.makeText(this, "Tarefa já cadastrada.", Toast.LENGTH_SHORT).show();
                     return aux;
                 }
@@ -61,14 +114,6 @@ public class MainActivity extends AppCompatActivity {
         aux = true;
         return aux;
     }
-    public void onRemoveClick(View view) {
-        tarefas.remove(0);
-        adapter.notifyDataSetChanged();
-        if(tarefas.isEmpty()){
-            btnRemove.setEnabled(false);
-        }
-    }
-
 
     protected void onSaveInstanceState(Bundle state)
     {
